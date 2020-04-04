6edd36426073011afb4cd6cc2b9fcba7854e34d9
Max Poletto <max.poletto@gmail.com>
2020-04-04 11:40:21 +0200

    Many changes: better dup elimination, fixed css, cleaned up comments. Changes output display to differentiate between inputs and intermediate results.

--- krypto/krypto.js
+++ krypto/krypto.js
@@ -1,37 +1,16 @@
 'use strict';
 
-var dbgTrees = true;
-var dbgExpr = true;
-var dbgSummary = true;
-var dbgSkip1 = false;
-var dbgSkip2 = false;
-var dbgSkip3 = false;
-
-var picks = [ 1, 2, 3, 4, 5, 6 ];
+var dbgTrees = false, dbgExpr = false, dbgSummary = false,
+    dbgSkip1 = false, dbgSkip2 = false, dbgSkip3 = false;
 
 // Generates a random integer in [0, max-1].
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-// Initializes a random set of playing cards on the Krypto game page.
-function initGame() {
-    var a = [ 1, 2, 3, 4, 5, 6 ],
-        b = [ 7, 8, 9, 10 ],
-        c = [ 11, 12, 13, 14, 15, 16, 17 ],
-        d = [ 18, 19, 20, 21, 22, 23, 24, 25 ];
-    var values = a.concat(a, a, b, b, b, b, c, c, d);
-    for (var i = 1; i < 7; i++) {
-        var n = getRandomInt(values.length),
-	    v = values.splice(n, 1)[0],
-            c = document.getElementById('c'+i);
-        c.innerHTML = v;
-	picks[i-1] = v;
-    }
-}
-
 // Generates all permutations of arr using Heap's iterative algorithm.
-// Sedgewick, R. "Permutation Generation Methods". ACM Computing Surveys 9(2), 1977.
+// Sedgewick, R. "Permutation Generation Methods".
+//    ACM Computing Surveys 9(2), 1977.
 // https://dl.acm.org/doi/10.1145/356689.356692
 // https://en.wikipedia.org/wiki/Heap%27s_algorithm
 function permutations(arr) {
@@ -100,8 +79,11 @@ function trees(n) {
 
 // Given an RPN arithmetic expression tree t and an array of integers
 // a, replaces the operand placeholders in t, in order, with the
-// values in a, and evaluates the corresponding expression. If any of
-// the intermediate results is negative or non-integer, immediately
+// values in a, and evaluates the corresponding expression.
+//
+// If any of the intermediate results is negative or non-integer, or
+// if an intermediate term would result in division by 0 or is 'x-0'
+// or 'x/1' (trivial variations of 'x+0' and 'x*1'), immediately
 // returns -1.
 function treeEval(t, a) {
     var s = [], i = 0;
@@ -131,21 +113,39 @@ function treeEval(t, a) {
     return s.pop()
 }
 
-// Pretty-prints the RPN expression t applied to the array of integers
-// a, as defined for treeEval() above, converting from RPN to a
-// series of in-order binary operations with intermediate results.
+// Pretty-prints the RPN tree t applied to the array of integers a, as
+// defined for treeEval() above, as a series of semicolon-separated
+// infix expressions.
+//
+// Returns an array of two strings: a simple one, and a "formatted"
+// one in which the original inputs (the values from a) are formatted
+// with an HTML span.
+//
+// Returns an empty array if the RPN tree contains a commutative
+// expression whose commutative version has already been seen (via a
+// different permutation of a).
 var exprSeen;
 var sb = '<span class="cc">', se = '</span>';
 function treePrint(t, a) {
-    var evalStk = [], dupStk = [], i = 0, res = "";
+    var evalStk = [],		// Implements RPN evaluation.
+	exprStk = [],		// Records intermediate terms as
+				// strings, for duplicate elimination.
+	fmtStk = [],		// Includes format info to distinguish
+				// inputs from intermediate results.
+	i = 0,			// Pointer into a.
+	res = "",		// Result string in infix notation.
+	fmtRes = "";		// Formatted result string.
     for (var e of t) {
 	if (e == 'x') {
 	    evalStk.push(a[i]);
-	    dupStk.push(a[i]);
+	    exprStk.push(a[i]);
+	    fmtStk.push(sb + a[i] + se);
 	    ++i;
 	} else {
-	    var y = evalStk.pop(), x = evalStk.pop(), r;
-	    var dy = dupStk.pop(), dx = dupStk.pop();
+	    var r;
+	    var y = evalStk.pop(), x = evalStk.pop();
+	    var ey = exprStk.pop(), ex = exprStk.pop();
+	    var fy = fmtStk.pop(), fx = fmtStk.pop();
 	    switch (e) {
 	    case '+': r = x+y; break;
 	    case '-': r = x-y; break;
@@ -154,25 +154,28 @@ function treePrint(t, a) {
 	    }
 	    var expr;
 	    if (e == '+' || e == '*') {
-		expr = [dy, dx, e].join(';');
+		expr = [ey, ex, e].join(';');
 		if (exprSeen.has(expr)) {
 		    if (dbgSkip2) {
 			console.log("Skipping @2 because " + expr + " exists");
 		    }
-		    return '';
+		    return [];
 		}
 	    }
-	    evalStk.push(r);
-	    expr = [dx, dy, e].join(';');
-	    dupStk.push(expr);
+	    expr = [ex, ey, e].join(';');
+	    exprStk.push(expr);
 	    exprSeen.add(expr);
+	    evalStk.push(r);
+	    fmtStk.push(r);
 	    res += x + e + y + "=" + r + ";";
+	    fmtRes += fx + e + fy + "=" + r + ";";
 	}
     }
     if (dbgExpr) {
-	console.log("Applied " + t + " to " + a + " to obtain " + dupStk.pop() + " / " + res);
+	console.log("Applied " + t + " to " + a + " to obtain " +
+		    exprStk.pop() + " / " + res);
     }
-    return res;
+    return [res, fmtRes];
 }
 
 // Given a set of integers nums and a target value goal, produces an
@@ -194,9 +197,31 @@ function solve(nums, goal) {
 	    if (n == goal) {
 		++nGoal;
 		var p = treePrint(t, a);
-		if (p.length > 0) {
-		    ans.push(p);
+		if (p.length == 0 || opsSeen.has(p[0])) {
+                    // More poor man's duplicate elimination.
+                    // Expression trees are distinct (commutative
+                    // duplicates removed) at this point, but two
+                    // non-commutative trees may still result in the
+                    // same series of operations given a different
+                    // permutation of inputs.
+		    //
+		    // Example:
+                    // RPN tree xxxx-/- applied to [4,2,3,1] and
+                    // RPN tree xxx-x/- applied to [4,3,1,2] both
+                    // result in operations 3-1=2, 2/2=1, 4-1=3.
+		    //
+		    // Note: this eliminates expressions that are
+		    // identical but differ in which terms are inputs
+		    // and which are intermediate results. That seems
+		    // ok: it looks better and it's easy enough to
+		    // derive the variants.
+                    if (dbgSkip3 && p.length > 0) {
+			console.log("Skipping @3 for " + p[0]);
+                    }
+                    continue;
 		}
+		opsSeen.add(p[0]);
+		ans.push(p[1]);
 	    }
 	}
     }
@@ -207,31 +232,27 @@ function solve(nums, goal) {
     return ans;
 }
 
-// Populates the 'answers' div of the Krypto game page with solutions.
-function showSolutions(solutions) {
-    var ansdiv = document.getElementById('ansdiv');
-    if (solutions.length == 0) {
-	ansdiv.innerHTML = "There are no solutions!"
-    } else {
-	var anstab = document.getElementById('anstab');
-	anstab.innerHTML = "";
-	solutions.sort();
-	for (var sol of solutions) {
-	    var terms = sol.split(';');
-	    terms.pop();
-	    var row = anstab.insertRow(-1);
-	    for (var term of terms) {
-		var cell = row.insertCell(-1);
-		cell.innerHTML = term;
-	    }
-	}
-	anstab.style.display = 'table';
-    }
-    ansdiv.style.display = 'block';
+function showNoSolutions() {
+    document.getElementById('anstab').style.display = 'none';
+    document.getElementById('noans').style.display = 'block';
 }
 
-function showAnswers() {
-    showSolutions(solve(picks.slice(0, picks.length-1), picks[picks.length-1]));
+// Populates the 'answers' div of the Krypto game page with solutions.
+function showSolutions(solutions) {
+    var anstab = document.getElementById('anstab');
+    anstab.innerHTML = "";
+    solutions.sort();
+    for (var sol of solutions) {
+	var terms = sol.split(';');
+	terms.pop();
+	var row = anstab.insertRow(-1);
+	for (var term of terms) {
+	    var cell = row.insertCell(-1);
+	    cell.innerHTML = term;
+	}
+    }
+    document.getElementById('anstab').style.display = 'block';
+    document.getElementById('noans').style.display = 'none';
 }
 
 function showError(msg) {
@@ -239,15 +260,39 @@ function showError(msg) {
     box.innerHTML = msg;
     box.style.display = 'block';
 }
+
 function hideError() {
-    var box = document.getElementById('error');
-    box.style.display = 'none';
+    document.getElementById('error').style.display = 'none';
+}
+
+function hideExplanation() {
+    document.getElementById('explanation').style.display = 'none';
+}
+function toggleExplanation() {
+    var x = document.getElementById("explanation");
+    if (x.style.display == 'block') {
+	x.style.display = 'none';
+    } else {
+	x.style.display = 'block';
+    }
+}
+
+function showAnswer(s) {
+    if (s.length == 0) {
+	showNoSolutions();
+    } else {
+	showSolutions(s);
+    }
+}
+
+function solveCards() {
+    showAnswer(solve(picks.slice(0, picks.length-1), picks[picks.length-1]));
 }
 
 var reCards = /^\s*\d+\s+\d+[\d\s]*$/,
     reObj = /\d+/,
     reSplit = /\s+/;
-function solveGame() {
+function solveInputs() {
     var cards = document.getElementById('cards'),
 	objective = document.getElementById('objective');
     if (!cards.value.match(reCards)) {
@@ -265,14 +310,23 @@ function solveGame() {
 	return;
     }
     hideError();
-    showSolutions(solve(cvals, oval));
+    hideExplanation();
+    showAnswer(solve(cvals, oval));
 }
 
-function toggleExplanation() {
-    var x = document.getElementById("explanation");
-    if (x.style.display == 'block') {
-	x.style.display = 'none';
-    } else {
-	x.style.display = 'block';
+// Initializes a random set of playing cards on the Krypto game page.
+var picks = [ 1, 2, 3, 4, 5, 6 ];
+function initGame() {
+    var a = [ 1, 2, 3, 4, 5, 6 ],
+        b = [ 7, 8, 9, 10 ],
+        c = [ 11, 12, 13, 14, 15, 16, 17 ],
+        d = [ 18, 19, 20, 21, 22, 23, 24, 25 ];
+    var values = a.concat(a, a, b, b, b, b, c, c, d);
+    for (var i = 1; i < 7; i++) {
+        var n = getRandomInt(values.length),
+	    v = values.splice(n, 1)[0],
+            c = document.getElementById('c'+i);
+        c.innerHTML = v;
+	picks[i-1] = v;
     }
 }
